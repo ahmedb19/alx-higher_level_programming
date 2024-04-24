fd0626642512802b99b8b5a02516dfd689102d24
Ahmed Bouali <ahmedbouali@proton.me>
2024-04-24 17:28:16 +0100

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 101):
+    for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
         else:
-            print("{:d}".format(i), " " if i < 101 else end="\n")
+            print("{:d}".format(i), end=" ")
