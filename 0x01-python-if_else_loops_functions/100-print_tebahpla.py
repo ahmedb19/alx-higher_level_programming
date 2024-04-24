90e7b4063567992b0ef376442da78663494ea5a2
Ahmed Bouali <ahmedbouali@proton.me>
2024-04-24 18:08:28 +0100

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1):
-    print("{:c}".format(i), end="")
+for i in range(25, -1, -1):
+    d = i + ord('A')
+    if i % 2 == 1:
+        d += 32
+    print("{:c}".format(d), end="")
